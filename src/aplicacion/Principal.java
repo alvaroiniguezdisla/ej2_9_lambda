c3ad585f048b0138f3ad6df54046fd66aa3fe3a9
Álvaro <ainiguezdislaa@gmail.com>
2023-02-21 17:00:08 +0100

    ej terminado

--- src/aplicacion/Principal.java
+++ src/aplicacion/Principal.java
@@ -1,2 +1,9 @@
-package aplicacion;public class Principal {
+package aplicacion;
+
+public class Principal {
+    public static void main(String[] args) {
+        int[] lista = {1,2,3,4,5,6,7,8,9,10};
+
+        System.out.println(dominio.ej2_9_lambda.nuevalistapares(lista));
+    }
 }
--- src/dominio/ej2_9_lambda.java
+++ src/dominio/ej2_9_lambda.java
@@ -1,2 +1,11 @@
-package dominio;public class ej2_9_lambda {
+package dominio;
+import java.util.ArrayList;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class ej2_9_lambda {
+    public static ArrayList nuevalistapares(int[] lista){
+        ArrayList listanueva = new ArrayList();
+      return Stream.iterate(0,i->i+1).limit(lista.length).map(i->lista[i]).filter(i->i%2==0).collect(Collectors.toCollection(ArrayList::new));
+    }
 }
